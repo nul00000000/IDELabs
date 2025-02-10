9802bf4bf56e84a6a2c675aab1555fbe4d3263e1
nul00000000 <nulcreatesgames@gmail.com>
2025-02-10 14:30:27 -0500

    pin setup

--- Lab5/Lab05.c
+++ Lab5/Lab05.c
@@ -9,3 +9,25 @@ Switch 1 Interrupt:
 Switch 2 Interrupt: 
 	Interrupt is P1IFG.1
 	*/
+	
+#include "msp.h"
+#include "Common.h"
+
+#define SW1 BIT1
+#define SW2 BIT4
+#define SWITCHES (SW1 | SW2)
+
+void setup() {
+	//set as GPIO
+	P1->SEL0 &= ~SWITCHES;
+	P1->SEL1 &= ~SWITCHES;
+	//set as input
+	P1->DIR &= ~SWITCHES;
+	//enable falling edge triggered interrupts
+	P1->IES |= SWITCHES;
+	P1->IE |= SWITCHES;
+}
+
+int main(void) {
+	
+}
