6ac778b9c471c51918f1b6953da20d3f13febb10
H-Tevin <tevinhendess@gmail.com>
2025-02-10 18:16:18 -0500

    nothing

--- Lab5/Lab05.c
+++ Lab5/Lab05.c
@@ -8,6 +8,24 @@ Switch 1 Interrupt:
 	Set falling edge with: 
 Switch 2 Interrupt: 
 	Interrupt is P1IFG.1
+
+00h T32LOAD1 - load register
+04h T32VALUE1 - current value register
+08h T32CONTROL1 - control register
+0Ch T32INTCLR1 - clear register
+
+20h T32LOAD2 - load register
+24h T32VALUE2 - current value register
+28h T32CONTROL2 - control register
+2Ch T32INTCLR2 - clear register
+
+Control:
+0000 0000 0000 0000 0000 0000 0000 0000
+XXXX XXXX XXXX XXXX XXXX XXXX 101X 0110
+Timer32_1->Control = 0x000000A6
+Timer32_2->Control = 0x000000A6
+
+https://faculty-web.msoe.edu/johnsontimoj/EE2920/files2920/timer_32.pdf
 	*/
 	
 #include "msp.h"
@@ -26,6 +44,10 @@ void setup() {
 	//enable falling edge triggered interrupts
 	P1->IES |= SWITCHES;
 	P1->IE |= SWITCHES;
+//------------------------------------------------
+	//Enable timers
+	Timer32_1->Control = 0x000000A6;
+	Timer32_2->Control = 0x000000A6;
 }
 
 int main(void) {
