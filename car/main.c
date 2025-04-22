4952ddf2ea09dc7e4140659129b6e1272be71675
nul00000000 <nulcreatesgames@gmail.com>
2025-04-22 17:08:57 -0400

    bigger derivative window

--- car/main.c
+++ car/main.c
@@ -5,15 +5,16 @@
 #include <stdio.h>
 
 //#define P (0.11f)
-#define P (0.13f)
+#define P (0.11f)
 #define I (0.0f)
 #define D (10.0f)
 //try cranking D way up
-#define DIFF_COEF (0.3f)
+#define DIFF_COEF (0.25f)
 
 #define STEERING_OFF (0.1f)
 
-#define NUM_MEM 25
+//having this higher seems to be gaming
+#define NUM_MEM 50
 
 extern float lightCenter;
 extern int num;
@@ -90,8 +91,8 @@ int main() {
 			setWheel(1, 0);
 		} else {
 			setSteerAngle(steering);
-			setWheel(0, (-0.55f + (diffDrive > 0 ? diffDrive * DIFF_COEF : 0)) * speed); //the polarity is now correct
-			setWheel(1, (-0.55f + (diffDrive < 0 ? -diffDrive * DIFF_COEF : 0)) * speed);
+			setWheel(0, (-0.47f + (diffDrive > 0 ? diffDrive * DIFF_COEF : 0)) * speed); //the polarity is now correct
+			setWheel(1, (-0.47f + (diffDrive < 0 ? -diffDrive * DIFF_COEF : 0)) * speed);
 			//setWheel(0, 0);
 			//setWheel(1, 0);
 			//we all live in a yellow submarine
